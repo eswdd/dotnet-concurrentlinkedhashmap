9759477e0028586a02aa56a058bcc2f57afdae74
Simon Matic Langford <simon@exemel.co.uk>
2014-05-30 08:17:03 +0100

    Added ConcurrentTestHarness helper class. Moved AtomicXXX implementations into their own source file

---
+++ Atomics.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading;
+
+namespace ConcurrentLinkedDictionary
+{
+
+	public class AtomicReference<T> where T : class
+	{
+		public AtomicReference() {}
+		public AtomicReference(T t) {
+			_value = t;
+		}
+
+		private T _value;
+
+		public T GetValue() {
+			// todo: cant find this!
+			return Volatile.Read<T>(ref _value);
+		}
+
+		// no lazy set in c# :(
+		public void LazySet (T i)
+		{
+			SetValue (i);
+		}
+
+
+		public void SetValue(T i) {
+			Interlocked.Exchange (ref _value, i);
+		}
+
+		public bool CompareAndSet (T compare, T newValue)
+		{
+			// todo: desperately need some atomicreference tests
+			T ret = Interlocked.CompareExchange (ref _value, newValue, compare);
+			return (ret == null && newValue == null) || (ret != null && Equals (ret, compare));
+		}
+
+	}
+
+	public class AtomicLong
+	{
+		long _value;
+
+		public AtomicLong() {
+		}
+		public AtomicLong(long v) {
+			_value = v;
+		}
+
+		public long GetValue() {
+			return Interlocked.Read (ref _value);
+		}
+
+		// no lazy set in c# :(
+		public void LazySet (long i)
+		{
+			SetValue (i);
+		}
+
+		public void SetValue(long i) {
+			Interlocked.Exchange (ref _value, i);
+		}
+
+
+	}
+
+	/*			*
+   * An AtomicReference with heuristic padding to lessen cache effects of this
+   * heavily CAS'ed location. While the padding adds noticeable space, the
+   * improved throughput outweighs using extra space.
+   */
+	class PaddedAtomicReference<T> : AtomicReference<T> where T : class {
+
+		// Improve likelihood of isolation on <= 64 byte cache lines
+		long q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, qa, qb, qc, qd, qe;
+
+		public PaddedAtomicReference() {}
+
+		public PaddedAtomicReference(T value) : base(value) {}
+	}
+
+	/*			*
+   * An AtomicLong with heuristic padding to lessen cache effects of this
+   * heavily CAS'ed location. While the padding adds noticeable space, the
+   * improved throughput outweighs using extra space.
+   */
+	public class PaddedAtomicLong : AtomicLong {
+
+		// Improve likelihood of isolation on <= 64 byte cache lines
+		long q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, qa, qb, qc, qd, qe;
+
+		public PaddedAtomicLong() {}
+
+		public PaddedAtomicLong(long value) : base(value) {
+		}
+	}
+	public class AtomicReferenceArray<T> where T : class
+	{
+		private T[] _array;
+
+		public AtomicReferenceArray(int length)
+		{
+			_array = new T[length];
+		}
+
+		public int Length {
+			get { return _array.Length; }
+		}
+
+		public T this [int index] {
+			get {
+				return Volatile.Read (ref _array [index]);
+			}
+			set {
+				Volatile.Write (ref _array [index], value);
+			}
+		}
+
+		// todo: need to complete the api
+	}
+}
+
---
+++ Test/ConcurrentTestHarness.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ConcurrentLinkedDictionary.Test
+{
+	/**
+ * A testing harness for concurrency related executions.
+ * <p/>
+ * This harness will ensure that all threads execute at the same time, records
+ * the full execution time, and optionally retrieves the responses from each
+ * thread. This harness can be used for performance tests, investigations of
+ * lock contention, etc.
+ * <p/>
+ * This code was adapted from <tt>Java Concurrency in Practice</tt>, using an
+ * example of a {@link CountDownLatch} for starting and stopping threads in
+ * timing tests.
+ *
+ * @author ben.manes@gmail.com (Ben Manes)
+ */
+	public sealed class ConcurrentTestHarness
+	{
+		private ConcurrentTestHarness() {
+			throw new InvalidOperationException("Cannot instantiate static class");
+		}
+
+		/**
+   * Executes a task, on N threads, all starting at the same time.
+   *
+   * @param nThreads the number of threads to execute
+   * @param task the task to execute in each thread
+   * @return the execution time for all threads to complete, in nanoseconds
+   */
+		public static long timeTasks(int nThreads, Action task) {
+			return timeTasks(nThreads, task, "Thread");
+		}
+
+		/**
+   * Executes a task, on N threads, all starting at the same time.
+   *
+   * @param nThreads the number of threads to execute
+   * @param task the task to execute in each thread
+   * @param baseThreadName the base name for each thread in this task set
+   * @return the execution time for all threads to complete, in nanoseconds
+   */
+		public static long timeTasks(int nThreads, Action task, String baseThreadName)
+		 {
+			return timeTasks<object>(nThreads, () => { task(); return null; }, baseThreadName).getExecutionTime();
+		}
+
+		/**
+   * Executes a task, on N threads, all starting at the same time.
+   *
+   * @param nThreads the number of threads to execute
+   * @param task the task to execute in each thread
+   * @return the result of each task and the full execution time, in nanoseconds
+   */
+		public static TestResult<T> timeTasks<T>(int nThreads, Func<T> task) where T : class
+		 {
+			return timeTasks(nThreads, task, "Thread");
+		}
+
+		/**
+   * Executes a task, on N threads, all starting at the same time.
+   *
+   * @param nThreads the number of threads to execute
+   * @param task the task to execute in each thread
+   * @param baseThreadName the base name for each thread in this task set
+   * @return the result of each task and the full execution time, in
+   *     nanoseconds
+   */
+		public static TestResult<T> timeTasks <T>(int nThreads, Func<T> task,
+			String baseThreadName) where T : class
+		 {
+			CountdownEvent startGate = new CountdownEvent(1);
+			CountdownEvent endGate = new CountdownEvent(nThreads);
+			AtomicReferenceArray<T> results = new AtomicReferenceArray<T>(nThreads);
+
+			IList<Thread> threads = new List<Thread>(nThreads);
+			for (int i = 0; i < nThreads; i++) {
+				int index = i;
+				var thread = new Thread (() => {
+					startGate.Wait();
+					try {
+						results[index] = task();
+					} finally {
+						endGate.Signal();
+					}
+				}) { Name = baseThreadName + "-" + i, IsBackground = true };
+				thread.Start();
+				threads.Add(thread);
+			}
+
+			var sw = Stopwatch.StartNew ();
+			startGate.Signal ();
+			endGate.Wait ();
+			return new TestResult<T>(sw.Elapsed.Ticks, toList(results));
+		}
+
+		/**
+   * Migrates the data from the atomic array to a {@link List} for easier
+   * consumption.
+   *
+   * @param data the per-thread results from the test
+   * @return the per-thread results as a standard collection
+   */
+		private static IList<T> toList<T>(AtomicReferenceArray<T> data) where T:class {
+			var list = new List<T>(data.Length);
+			for (int i = 0; i < data.Length; i++) {
+				list.Add(data[i]);
+			}
+			return list;
+		}
+
+		/**
+   * The results of the test harness's execution.
+   *
+   * @param <T> the data type produced by the task
+   */
+		public class TestResult<T> {
+			private readonly long executionTime;
+			private readonly IList<T> results;
+
+			public TestResult(long executionTime, IList<T> results) {
+				this.executionTime = executionTime;
+				this.results = results;
+			}
+
+			/**
+     * The test's execution time, in nanoseconds.
+     *
+     * @return The time to complete the test.
+     */
+			public long getExecutionTime() {
+				return executionTime;
+			}
+
+			/**
+     * The results from executing the tasks.
+     *
+     * @return The outputs from the tasks.
+     */
+			public IList<T> getResults() {
+				return results;
+			}
+		}
+	}
+}
+
